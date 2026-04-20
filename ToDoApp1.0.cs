7b3ea1bc4fa3be655721e1bf69b31e758322ed16
DarkWine <162964772+DarkWine01@users.noreply.github.com>
2026-04-21 01:14:54 +0300

    Add files via upload

---
+++ ToDoApp1.0.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Список дел");
+
+        List<string> tasks = new List<string>()
+        {
+            "Сделать уроки",
+            "Купить продукты",
+            "Позвонить другу"
+        };
+
+        foreach (string task in tasks)
+        {
+            Console.WriteLine("- " + task);
+        }
+    }
+}
+
+
